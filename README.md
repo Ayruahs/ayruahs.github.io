ad989873698a11e665fa4ab646c0bd72dd1b4078
Shaurya Sinha <shauryasinna@gmail.com>
2019-09-29 01:46:33 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# ayruahs.github.io
-
-Hey, welcome to my personal website! It is still a work in progress and I'm continually updating the projects section but feel free to look around.
---
+++ precache-manifest.6aa8b4a974a54c8897115cb18de7671f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cad36a9ce15b05388f611f091510e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b4fb786aacb4544608e",
+    "url": "/static/css/main.fbb410d5.chunk.css"
+  },
+  {
+    "revision": "1e393f424661f2618efc",
+    "url": "/static/js/2.fc758d29.chunk.js"
+  },
+  {
+    "revision": "1b4fb786aacb4544608e",
+    "url": "/static/js/main.e438e082.chunk.js"
+  },
+  {
+    "revision": "c39099addc434270482f",
+    "url": "/static/js/runtime~main.82eca2a1.js"
+  },
+  {
+    "revision": "b3b06989cdbd59e51633319a5be7851b",
+    "url": "/static/media/Avenir Roman.b3b06989.otf"
+  },
+  {
+    "revision": "d227e2d31cf9520165d6469e5fa5060a",
+    "url": "/static/media/Shaurya_Sinha_resume.d227e2d3.pdf"
+  },
+  {
+    "revision": "1ec6550a999a94ff2c53c225e953e204",
+    "url": "/static/media/baker_beach3.1ec6550a.jpg"
+  },
+  {
+    "revision": "70d7d0c78ab40672136c1cca7486c2a2",
+    "url": "/static/media/dolores.70d7d0c7.jpg"
+  },
+  {
+    "revision": "1645a8186ac51747ba5d7920d3c2b68e",
+    "url": "/static/media/fulcrum.1645a818.png"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/glyph-logo_May2016.0e84d592.png"
+  }
+]);
